c58edb30f27fd4a68d4cef7eedc502521427c07e
Eweer <oriol253@gmail.com>
2016-04-24 21:16:11 +0200

    Postgame
    Added all the stuff related to the scores.
    Ranking is still pending to show up.
    CARE. Mem leak on keyboard + Heap error when exiting with escape in
    title screen after the keyboard input

--- ModuleNameScreen.h
+++ ModuleNameScreen.h
@@ -4,8 +4,15 @@
 #include "Module.h"
 #include "Animation.h"
 #include "Globals.h"
+#include "dynArray.h"
+#include "ModuleUI.h"
 
 struct SDL_Texture;
+
+struct Ranking {
+	char* name;
+	int area, Tscore;
+};
 
 class ModuleNameScreen : public Module
 {
@@ -16,10 +23,23 @@ public:
 	bool Start();
 	update_status Update();
 	bool CleanUp();
+	void UpdateScores();
+	void InputName();
+	bool TopScore();
 
 public:
 
+	char* keyboard;
+	int x = 0, y = 0;
+	int letter = 0;
+	int square_x;
+	int square_y;
+	int rank;
+	Animation square;
+	dynArray<Ranking> TopScores;
 	SDL_Texture* graphics = nullptr;
+	SDL_Texture* layout_graphic = nullptr;
+	SDL_Rect layout;
 	SDL_Rect name_screen;
 
 };
--- ModuleScoreScreen.h
+++ ModuleScoreScreen.h
@@ -4,7 +4,10 @@
 #include "Module.h"
 #include "Animation.h"
 #include "Globals.h"
+
+
 struct SDL_Texture;
+
 
 class ModuleScoreScreen : public Module
 {
@@ -16,7 +19,10 @@ public:
 	update_status Update();
 	bool CleanUp();
 
+
+
 public:
+
 
 	SDL_Texture* graphics = nullptr;
 	SDL_Rect score_screen;
