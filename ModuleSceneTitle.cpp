9fa13a4520c18a4711cb2bb3f3407992d59e9d65
Sergi Pérez <sergiperezandreu@gmail.com>
2016-04-25 10:25:47 +0200

    The O char works, added all the other positions of the chars

--- ModuleSceneTitle.cpp
+++ ModuleSceneTitle.cpp
@@ -33,11 +33,93 @@ ModuleSceneTitle::ModuleSceneTitle()
 
 	title_anim.speed = 0.08f;
 	title_anim.loop = false;
+	//char O
+	chars_rec_o.x = 1;
+	chars_rec_o.y = 60;
+	chars_rec_o.w = 36;
+	chars_rec_o.h = 37;
 	//
-	chars_rec_o.x = 0;
-	chars_rec_o.y = 0;
-	chars_rec_o.w = 72;
-	chars_rec_o.h = 66;
+	//char u
+	chars_rec_u.x = 40;
+	chars_rec_u.y = 67;
+	chars_rec_u.w = 30;
+	chars_rec_u.h = 28;
+	//
+	//char t
+	chars_rec_t.x = 74;
+	chars_rec_t.y = 57;
+	chars_rec_t.w = 48;
+	chars_rec_t.h = 43;
+	//
+	//char z
+	chars_rec_z.x = 125;
+	chars_rec_z.y = 59;
+	chars_rec_z.w = 45;
+	chars_rec_z.h = 39;
+	//
+	//char o2
+	chars_rec_o2.x = 173;
+	chars_rec_o2.y = 65;
+	chars_rec_o2.w = 29;
+	chars_rec_o2.h = 33;
+	//
+	//char n
+	chars_rec_n.x = 205;
+	chars_rec_n.y = 64;
+	chars_rec_n.w = 32;
+	chars_rec_n.h = 33;
+	//
+	//char e
+	chars_rec_e.x = 240;
+	chars_rec_e.y = 66;
+	chars_rec_e.w = 31;
+	chars_rec_e.h = 34;
+	//
+	// Here will be the bright chars
+
+	//char Ob
+	chars_rec_ob.x = 1;
+	chars_rec_ob.y = 10;
+	chars_rec_ob.w = 36;
+	chars_rec_ob.h = 38;
+	//
+	//char ub
+	chars_rec_ub.x = 40;
+	chars_rec_ub.y = 19;
+	chars_rec_ub.w = 30;
+	chars_rec_ub.h = 28;
+	//
+	//char tb
+	chars_rec_tb.x = 74;
+	chars_rec_tb.y = 6;
+	chars_rec_tb.w = 48;
+	chars_rec_tb.h = 43;
+	//
+	//char zb
+	chars_rec_zb.x = 125;
+	chars_rec_zb.y = 9;
+	chars_rec_zb.w = 45;
+	chars_rec_zb.h = 39;
+	//
+	//char o2b
+	chars_rec_o2b.x = 173;
+	chars_rec_o2b.y = 14;
+	chars_rec_o2b.w = 29;
+	chars_rec_o2b.h = 33;
+	//
+	//char nb
+	chars_rec_nb.x = 205;
+	chars_rec_nb.y = 15;
+	chars_rec_nb.w = 32;
+	chars_rec_nb.h = 33;
+	//
+	//char eb
+	chars_rec_eb.x = 240;
+	chars_rec_eb.y = 15;
+	chars_rec_eb.w = 30;
+	chars_rec_eb.h = 33;
+	//
+
 }
 
 ModuleSceneTitle::~ModuleSceneTitle()
@@ -47,14 +129,17 @@ ModuleSceneTitle::~ModuleSceneTitle()
 bool ModuleSceneTitle::Start()
 {
 	LOG("Loading Title scene");
-	chars_tex = App->textures->Load("Outzone/Sprites/Title/title_letterss.png");
+	chars_tex = App->textures->Load("Outzone/Sprites/Title/title_letters.png");
 	title_tex = App->textures->Load("OutZone/Sprites/Title/title_name_anim.png");
 	App->player->Disable();
 	App->ui->title = true;
 	starttime = SDL_GetTicks();
-	//Char o
-	ox = 30;
-	oy = 200;
+	//Char ob
+	obx = 147;
+	oby = 358;
+	//
+	//Char ub
+	
 	//
 	return true;
 }
@@ -74,25 +159,27 @@ bool ModuleSceneTitle::CleanUp()
 update_status ModuleSceneTitle::Update()
 {
 	int currenttime = SDL_GetTicks();
-	// Draw everything --------------------------------------
-	App->render->Blit(title_tex, 17, 89, &title_anim.GetCurrentFrame()); // background
-	
-	//Char o
-	if (currenttime > starttime + 0 && currenttime < starttime + 1000){
-		oy -= 2;
-		ox += 1;
+
+		//Char o
+	if (currenttime<(starttime + 5500)){
+
+		if (currenttime > starttime + 0 && currenttime < starttime + 765){// 30 ticks per sec
+			oby -= 6;
+			obx -= 3;
+			currenttime = SDL_GetTicks();
+		}
+
+		App->render->Blit(chars_tex, obx, oby, &chars_rec_ob);
 	}
 	
-	App->render->Blit(chars_tex, ox, oy, &chars_rec_o);
-	//
-
+	else App->render->Blit(title_tex, 15, 87, &title_anim.GetCurrentFrame());
 	// swap Scene
 	if (App->input->keyboard[SDL_SCANCODE_SPACE] == KEY_STATE::KEY_DOWN && App->ui->credit>0){
 		App->fade->FadeToBlack(this, (Module*)App->levels, 1.0f);
 		App->ui->SubCoin();
 	}
 	
-
+	
 
 	return UPDATE_CONTINUE;
 }
--- ModuleSceneTitle.h
+++ ModuleSceneTitle.h
@@ -30,6 +30,60 @@ public:
 	int ox, oy;
 	SDL_Rect chars_rec_o;
 	//
+	//Char U
+	int ux, uy;
+	SDL_Rect chars_rec_u;
+	//
+	//Char T
+	int tx, ty;
+	SDL_Rect chars_rec_t;
+	//
+	//Char Z
+	int zx, zy;
+	SDL_Rect chars_rec_z;
+	//
+	//Char o2
+	int o2x, o2y;
+	SDL_Rect chars_rec_o2;
+	//
+	//Char n
+	int nx, ny;
+	SDL_Rect chars_rec_n;
+	//
+	//Char e
+	int ex, ey;
+	SDL_Rect chars_rec_e;
+	//
+	//THIS WILL BE THE BRIGHT ONES
+	//Char ob
+	int obx, oby;
+	SDL_Rect chars_rec_ob;
+	//
+	//Char Ub
+	int ubx, uby;
+	SDL_Rect chars_rec_ub;
+	//
+	//Char Tb
+	int tbx, tby;
+	SDL_Rect chars_rec_tb;
+	//
+	//Char Zb
+	int zbx, zby;
+	SDL_Rect chars_rec_zb;
+	//
+	//Char o2b
+	int o2bx, o2by;
+	SDL_Rect chars_rec_o2b;
+	//
+	//Char nb
+	int nbx, nby;
+	SDL_Rect chars_rec_nb;
+	//
+	//Char eb
+	int ebx, eby;
+	SDL_Rect chars_rec_eb;
+	//
+
 
 };
 
