97b39fe135572e88a8626cd70cb0e131b4eeefec
Trodek <ibanmas@gmail.com>
2016-06-02 06:00:38 +0200

    unload sounds

--- ModuleEnemies.cpp
+++ ModuleEnemies.cpp
@@ -117,7 +117,11 @@ update_status ModuleEnemies::PostUpdate()
 bool ModuleEnemies::CleanUp()
 {
 	LOG("Freeing all enemies");
-
+	App->audio->UnloadSoundEffect(expand_energy);
+	App->audio->UnloadSoundEffect(pick_energy);
+	App->audio->UnloadSoundEffect(pick_speed);
+	App->audio->UnloadSoundEffect(change_weapon);
+	App->audio->UnloadSoundEffect(enemy_hitted);
 	for(uint i = 0; i < MAX_ENEMIES; ++i)
 	{
 		if(enemies[i] != nullptr)
